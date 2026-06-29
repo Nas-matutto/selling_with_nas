2e5cd45a55e513eb39af1a7abccede0163e3700f
Nas <nasser.m.95@gmail.com>
2026-06-29 11:46:47 +0100

    guide v3

--- .vercel/output/_functions/chunks/content-modules_C3Yf-nip.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/data/post/how-much-to-charge-for-your-ecommerce-products.mdx", () => import('./how-much-to-charge-for-your-ecommerce-products_D7MrV4PH.mjs')]]);
-
-export { contentModules as default };
--- .vercel/output/_functions/chunks/how-much-to-charge-for-your-ecommerce-products_D7MrV4PH.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./how-much-to-charge-for-your-ecommerce-products_C708Ab1S.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
